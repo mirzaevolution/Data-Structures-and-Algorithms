85251a08ee91778d17094438795ff5b141debc73
Mirza Ghulam Rasyid <ghulamcyber@hotmail.com>
2017-10-24 11:04:44 +0700

    Adding Bubble sort and Insertion sort algorithms

---
+++ BubbleSort/BubbleSort.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BubbleSort
+{
+    /// <summary>
+    /// Class that holds extension method for performing Bubble Sort operation.
+    /// </summary>
+    public static class SortAlgorithm
+    {
+        private static void Swap<T>(T[] array, int previousIndex, int nextIndex)
+        {
+            if (previousIndex != nextIndex)
+            {
+                T item = array[previousIndex];
+                array[previousIndex] = array[nextIndex];
+                array[nextIndex] = item;
+            }
+        }
+        /// <summary>
+        /// Extension method to sort an array with Bubble Sort algorithm.
+        /// </summary>
+        /// <typeparam name="T">Any generic type that implements IComparable of T interface.</typeparam>
+        /// <param name="array">Array to sort</param>
+        public static void BubbleSort<T>(this T[] array)
+            where T : IComparable<T>
+        {
+            if (array == null || array.Length == 0)
+                return;
+            bool swapped;
+            int len = array.Length;
+            do
+            {
+                swapped = false;
+                for (int i = 1; i < len; i++)
+                {
+                    if (array[i - 1].CompareTo(array[i]) > 0)
+                    {
+                        Swap(array, i - 1, i);
+                        swapped = true;
+                    }
+                }
+            } while (swapped);
+        }
+
+    }
+}
---
+++ InsertionSort/InsertionSort.cs
@@ -0,0 +1,45 @@
+﻿using System;
+namespace InsertionSort
+{
+    /// <summary>
+    /// Class that holds extension method for performing Insertion Sort operation.
+    /// </summary>
+    public static class SortAlgorithm
+    {
+        /// <summary>
+        /// Extension method to sort an array with Insertion Sort algorithm.
+        /// </summary>
+        /// <typeparam name="T">Any generic type that implements IComparable of T interface.</typeparam>
+        /// <param name="array">Array to sort</param>
+        public static T[] InsertionSort<T>(this T[] array) where T : IComparable<T>
+        {
+            if (array == null || array.Length == 0)
+                return null;
+            int len = array.Length;
+            T[] newArray = new T[len];
+            newArray[0] = array[0];
+            int total = 1;
+            for (int index = 1; index < len; index++)
+            {
+                bool inserted = false;
+                for (int newIndex = 0; newIndex < total; newIndex++)
+                {
+                    if (newArray[newIndex].CompareTo(array[index]) > 0)
+                    {
+                        inserted = true;
+                        Array.Copy(newArray, newIndex, newArray, newIndex + 1, total - newIndex);
+                        newArray[newIndex] = array[index];
+                        total++;
+                    }
+                    if (inserted)
+                        break;
+                }
+                if (!inserted)
+                {
+                    newArray[total++] = array[index];
+                }
+            }
+            return newArray;
+        }
+    }
+}
