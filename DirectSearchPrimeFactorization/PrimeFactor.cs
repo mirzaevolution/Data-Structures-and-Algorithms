f0993758311890069a8b3eb8eed569e914e20511
Mirza Ghulam Rasyid <ghulamcyber@hotmail.com>
2018-01-02 08:30:31 +0700

    Adding Direct Search Prime Factorization

---
+++ DirectSearchPrimeFactorization/PrimeFactor.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DirectSearchPrimeFactorization
+{
+    public class PrimeFactor
+    {
+        private static bool IsPrime(uint number)
+        {
+            if (number <= 1)
+                return false;
+            if (number == 2)
+                return true;
+            uint bound = (uint)Math.Ceiling(Math.Sqrt(number));
+            for (int j = 2; j < bound; j++)
+            {
+                if (number % j == 0) return false;
+            }
+            return true;
+        }
+        public static IEnumerable<uint> FindPrimeFactors(uint number)
+        {
+            List<uint> numbers = new List<uint>();
+            List<uint> primes = new List<uint>();
+            for (uint i = 2; i <= number; i++)
+            {
+                if (IsPrime(2))
+                {
+                    primes.Add(i);
+                }
+            }
+            int index = 0;
+            while (number != 1)
+            {
+                if (number % primes[index] == 0)
+                {
+                    numbers.Add(primes[index]);
+                    number /= primes[index];
+                }
+                else
+                    index++;
+            }
+            return numbers;
+        }
+    }
+}
